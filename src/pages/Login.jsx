327e0282d4e8f092d374b7e0965f03d1d42f80b4
natalia <ntla.brs@gmail.com>
2023-02-09 16:43:31 -0300

    Requisito 1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,64 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      name: '',
+      isDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.validation());
+  };
+
+  validation = () => {
+    const { email, name } = this.state;
+    const minCaracteres = 0;
+    const verifyEmail = /\S+@\S+.\S+/;
+    if (name.length > minCaracteres && email.match(verifyEmail)) {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  };
+
   render() {
+    const { email, name, isDisabled } = this.state;
     return (
-      <h1>Login</h1>
+      <div>
+        <label>
+          <input
+            type="email"
+            name="email"
+            data-testid="input-gravatar-email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label>
+          <input
+            type="text"
+            name="name"
+            data-testid="input-player-name"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ isDisabled }
+        >
+          Login
+        </button>
+      </div>
     );
   }
 }
