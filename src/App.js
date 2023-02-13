1c4f5a071eac14ccf00df06f3d023adfbbe492a2
felipe <werer522p@gmail.com>
2023-02-13 16:02:43 -0300

    Feedbackpage Felipe

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import { Login, Game, Config } from './pages';
-// s
+import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
+
 export default function App() {
   return (
     <div className="App">
@@ -10,6 +12,8 @@ export default function App() {
         <Route exact path="/" component={ Login } />
         <Route path="/game" component={ Game } />
         <Route path="/config" component={ Config } />
+        <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     </div>
   );
