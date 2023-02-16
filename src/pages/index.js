ae638acef6c167a59f76333d845b620a3fdfb458
TulioKaaz <tuliocaz.windcoast@gmail.com>
2023-02-16 16:30:33 -0300

    style ranking page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import Game from './game/Game';
 import Login from './login/Login';
 import Config from './Config';
 import Feedback from './feedback/Feedback';
-import Ranking from './Ranking';
+import Ranking from './ranking/Ranking';
 
 export { Game, Login, Config, Feedback, Ranking };
